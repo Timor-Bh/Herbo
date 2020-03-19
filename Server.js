2e271368cfadbbb6823eb057fba67b39f2bc90b1
Timor-Bh <ahmed.bhouri@gmail.com>
2020-03-19 19:08:53 +0100

    MentorNations/DemoDay

--- Server.js
+++ Server.js
@@ -1,7 +1,7 @@
 const http = require("http");
-const port = process.env.port||8118;
+const port = process.env.PORT || 5000;
 const app = require("./app");
 const server = http.createServer(app);
 server.listen(port,function(){
-    console.log("app running on port 8118")
+    console.log("app running on port 8118");
 });
